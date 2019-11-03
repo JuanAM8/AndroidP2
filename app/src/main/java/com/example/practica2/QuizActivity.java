8bea7faec2b4a51f7fb816e1707a48a7608946fa
César Carbajo <cesares10@gmail.com>
2019-11-03 14:34:14 +0100

    Quiz mejorado, transicion de audios arreglado y randomizador de preguntas mejor implementado

--- app/src/main/java/com/example/practica2/QuizActivity.java
+++ app/src/main/java/com/example/practica2/QuizActivity.java
@@ -15,8 +15,17 @@ import android.widget.VideoView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.lang.reflect.Field;
+import java.util.Stack;
 /*Se desarrollara toda la partida, mostrando las preguntas, respondiendolas y gestionando el fin de juego.*/
 
+//TODO:Arreglar peso videos y musica
+//TODO:Seleccionar numero especifico de preguntas
+//TODO:Mostrar errores y aciertos
+//TODO:Finalizar una partida
+//TODO:Resto de pantallas y transiciones (seleccionar set)
+//TODO:Estetica
+//TODO:Desafios plata y oro
+//TODO:Cambiar xml a base de datos
 public class QuizActivity extends AppCompatActivity {
 
     protected VideoView videoView;
@@ -28,11 +37,24 @@ public class QuizActivity extends AppCompatActivity {
     protected RadioButton button2;
     protected RadioButton button3;
     protected RadioButton button4;
+    protected Button buttonConfirm;
+    protected MediaPlayer mediaPlayer;
+
+    protected Stack<String> currentQuestionStack = new Stack<String>();
+
+    protected boolean audioInit = false;
+
+    protected int nCorrect = 0;
+    protected int nWrong = 0;
+
+    protected int currentCorrectId = -1;
+    protected int currentAnswerId = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+        //Inicializacion de elementos del layout
         videoView = findViewById(R.id.video);
         buttonAudio = findViewById(R.id.audioButton);
         image = findViewById(R.id.image);
@@ -42,12 +64,36 @@ public class QuizActivity extends AppCompatActivity {
         button2 = findViewById(R.id.radio_R2);
         button3 = findViewById(R.id.radio_R3);
         button4 = findViewById(R.id.radio_R4);
-    }
-
-    protected void onResume(){
-        super.onResume();
+        //Listener del RadioButton
+        questionRadio.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                radioListener(group, checkedId);
+            }
+        });
+        buttonConfirm = findViewById(R.id.buttonConfirm);
+        //Listener del boton de confirmar
+        buttonConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAnswerResult();
+                questionRadio.clearCheck();
+                if(audioInit){
+                    mediaPlayer.release();
+                }
+                try {
+                    showRandomQuestion();
+                } catch (NoSuchFieldException e) {
+                    e.printStackTrace();
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        //Inicia primera pregunta
         try {
-            showRandomQuestion(R.array.questionsMovies);
+            initQuestions(R.array.questionsMovies);
+            showRandomQuestion();
         } catch (NoSuchFieldException e) {
             e.printStackTrace();
         } catch (IllegalAccessException e) {
@@ -56,12 +102,17 @@ public class QuizActivity extends AppCompatActivity {
     }
 
     public void createAudio(int audioId){
-        final MediaPlayer mediaPlayer = MediaPlayer.create(QuizActivity.this, audioId);
+        audioInit = true;
+        mediaPlayer = MediaPlayer.create(QuizActivity.this, audioId);
 
         buttonAudio.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mediaPlayer.start();
+                if(mediaPlayer.isPlaying()){
+                    mediaPlayer.pause();
+                }else {
+                    mediaPlayer.start();
+                }
             }
         });
         buttonAudio.setVisibility(View.VISIBLE);
@@ -79,19 +130,47 @@ public class QuizActivity extends AppCompatActivity {
         image.setVisibility(View.VISIBLE);
     }
 
-    public void showRandomQuestion(int setId) throws NoSuchFieldException, IllegalAccessException {
+    public void initQuestions(int setId){
+        String[] setQuestions = getResources().getStringArray(setId);
+        for(int i = 0; i < setQuestions.length; i++){
+            String tmp = setQuestions[i];
+            int rnd = (int) Math.round(Math.random()*(setQuestions.length - i) + i);
+            setQuestions[i] = setQuestions[rnd];
+            setQuestions[rnd] = tmp;
+        }
+        for (String s : setQuestions) {
+            currentQuestionStack.push(s);
+        }
+    }
+
+    public void showRandomQuestion() throws NoSuchFieldException, IllegalAccessException {
+        buttonConfirm.setVisibility(View.GONE);
         videoView.setVisibility(View.GONE);
         buttonAudio.setVisibility(View.GONE);
         image.setVisibility(View.GONE);
+        /*
         String[] setQuestions = getResources().getStringArray(setId);
+
         int random = (int) Math.round(Math.random() * (setQuestions.length - 1));
         String randomQuestion = setQuestions[random];
-        String[] parts = randomQuestion.split(";");
+        String[] parts = randomQuestion.split(";");*/
+        String currentQuestion = currentQuestionStack.pop();
+        String[] parts = currentQuestion.split(";");
+
         questionText.setText(parts[2]);
-        button1.setText(parts[3]);
-        button2.setText(parts[4]);
-        button3.setText(parts[5]);
-        button4.setText(parts[6]);
+        //.substring(1)
+        String[] answerText = new String[4];
+        for(int i = 0; i < 4; i++){
+            answerText[i] = parts[i + 3];
+            if (answerText[i].charAt(0) == '*'){
+                answerText[i] = answerText[i].substring(1);
+                currentAnswerId = i;
+            }
+        }
+        button1.setText(answerText[0]);
+        button2.setText(answerText[1]);
+        button3.setText(answerText[2]);
+        button4.setText(answerText[3]);
         switch (parts[1]){
             case "video":
                 Field videoField = R.raw.class.getDeclaredField(parts[0]);
@@ -107,6 +186,19 @@ public class QuizActivity extends AppCompatActivity {
                 Field imageField = R.drawable.class.getDeclaredField(parts[0]);
                 int imageId = imageField.getInt(imageField);
                 createImage(imageId);
+        }
+    }
+
+    public void radioListener(RadioGroup group, int checkedId){
+        buttonConfirm.setVisibility(View.VISIBLE);
+    }
+
+    public void checkAnswerResult(){
+        currentAnswerId = questionRadio.getCheckedRadioButtonId();
+        if(currentAnswerId == currentCorrectId){
+            nCorrect++;
+        }else{
+            nWrong++;
         }
     }
 }
